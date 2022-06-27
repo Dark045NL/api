9c5548020391c6add9d6bc835867db3900339b9f
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2022-06-27 23:43:31 +0100

    just sussy ipgrabber

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 require('dotenv').config();
 const express = require('express');
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 const port = 8080;
 const app = express();
 app.use(express.json());
 
-mongoose.connect(
-    process.env.LOGIN,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }
-);
+// mongoose.connect(
+//     process.env.LOGIN,
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     }
+// );
 
-const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-    console.log("Connected successfully");
-});
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+//     console.log("Connected successfully");
+// });
 
 app.get("/", (req, res) => {
     let ipAddr = req.headers["x-forwarded-for"];
