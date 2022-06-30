35d8743e345faff6479111dfb9876aeeff41f156
Diogo Matos <diogo.s.matos@tecnico.ulisboa.pt>
2022-06-30 19:06:49 +0100

    endpoints created for gettin all, creating game and udating votes

--- connection.js
+++ connection.js
@@ -1,13 +1,14 @@
 const { MongoClient } = require('mongodb');
 require('dotenv').config();
 
-async function main() {
+async function connection() {
 
     const uri = process.env.LOGIN;
 
     const client = new MongoClient(uri);
     try {
         await client.connect();
+        console.log("Connected correctly to server");
 
         //await creategames(client, {
         //    game: "Leauge of Legends",
@@ -17,19 +18,18 @@ async function main() {
 
         //await findOneListingByName(client, "Rocket League");
 
-        await updateListingByName(client, "Fortnite", { likes: 70 });
-
+       // await updateListingByName(client, "Fortnite", { likes: 70 });
+       
+       return client;
     } catch (e) {
         console.error(e);
-    } finally {
-        await client.close();
     }
 }
 
-main().catch(console.error);
+//connection().catch(console.error);
 
-async function updateListingByName(client, name, likes, dislikes) {
-    const result = await client.db("pollsystem").collection("games").updateOne({ game: name }, { $set: { likes: likes } });
+async function updateListingByName(client, name, likes, dislikes, voterIps) {
+    const result = await client.db("pollsystem").collection("games").updateOne({ game: name }, { $set: { likes: likes, dislikes: dislikes, voterIps: voterIps } });
 
     if (result) {
         console.log(`Updated game with the following name: ${name}`);
@@ -38,23 +38,32 @@ async function updateListingByName(client, name, likes, dislikes) {
     }
 }
 
-//async function findOneListingByName(client, name) {
-//    const result = await client.db("pollsystem").collection("games").findOne({ game: name });
-//
-//    if (result) {
-//        console.log(`Found game with the following name: ${name}`);
-//        console.log(result);
-//    } else {
-//        console.log(`No game found with the name: ${name}`);
-//    }
-//}
+async function findOneListingByName(client, name) {
+   const result = await client.db("pollsystem").collection("games").findOne({ game: name });
 
-//async function creategames(client, newgame) {
-//    const result = await client.db("pollsystem").collection("games").insertOne(newgame);
-//
-//    console.log(`Inserted game with the following id: ${result.insertedId}`);
-//}
+   if (result) {
+       console.log(`Found game with the following name: ${name}`);
+       console.log(result);
+   } else {
+       console.log(`No game found with the name: ${name}`);
+   }
+}
 
+async function createGames(client, newgame) {
+   const result = await client.db("pollsystem").collection("games").insertOne(newgame);
+
+   console.log(`Inserted game with the following id: ${result.insertedId}`);
+
+   return result;
+}
+
+async function findAllGames(client) {
+    const result = await client.db("pollsystem").collection("games").find({}).toArray();
+
+    console.log(`Found ${result.length} games`);
+
+    return result;
+}
 //async function listDatabases(client) {
 //    const databasesList = await client.db().admin().listDatabases();
 //
@@ -63,3 +72,5 @@ async function updateListingByName(client, name, likes, dislikes) {
 //        console.log(` - ${db.name}`);
 //    })
 //}
+
+module.exports={connection, createGames, findAllGames, updateListingByName, findOneListingByName};
--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 require('dotenv').config();
-const { MongoClient } = require('mongodb');
 const express = require('express');
-const port = 8080;
+
+const { connection, createGames, findAllGames,updateListingByName,findOneListingByName } = require('./connection');
+
 const app = express();
 app.use(express.json());
+
+let client;
+(async () => {
+    client = await connection();
+  })();
+
+app.use(async (req,res,next) => {
+    req.db = client;
+    next();
+});
 
 app.get("/", (req, res) => {
     let ipAddr = req.headers["x-forwarded-for"];
@@ -15,6 +26,61 @@ app.get("/", (req, res) => {
     }
     res.send(ipAddr);
 });
+
+//get all games
+app.get("/games", async (req, res) => {
+    try {
+        const games = await findAllGames(req.db);
+        res.json(games);
+    } catch (e) {
+        console.log(client)
+        console.error(e);
+        res.sendStatus(400);
+    }
+});
+
+//create new game on db
+app.post("/games", async (req, res) => {
+    //put here restrictions about name
+    if (req.body && req.body.name) {
+        try {
+            const game = await createGames(req.db, {name:req.body.name, likes:0, dislikes:0, voterIps:[]});
+            res.json(game);
+        } catch {
+            res.sendStatus(400);
+        }
+       
+    } else {
+        res.sendStatus(400);
+    }
+});
+
+//update game votes
+app.put("/game/:name", async (req, res) => {
+    if (req.body && req.body.likes && req.body.dislikes) {
+        try {
+            const checkIp = await findOneListingByName(req.db, req.params.name);
+            let ipAddr = req.headers["x-forwarded-for"];
+            if (ipAddr) {
+                const list = ipAddr.split(",");
+                ipAddr = list[list.length - 1];
+            } else {
+                ipAddr = req.connection.remoteAddress;
+            }
+
+            if (checkIp.voterIps.includes(ipAddr)) {
+                res.sendStatus(401);
+                return;
+            } else {
+                const game = await updateGame(req.db, req.params.name, req.body.likes, req.body.dislikes, [...checkIp.voterIps, ipAddr]);
+                res.json(game);
+            }
+        } catch {
+            res.sendStatus(400);
+        }
+    }
+});
+
 
 app.listen(process.env.PORT, () => {
     console.log("Server kaya is gayy");
